fb2b5f38c11bf620368c2c8357d4ae7fff77a1e1
Philipkioko19-pixel <philipkioko19@example.com>
2026-09-02 22:06:18 +0300

    Finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,13 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }

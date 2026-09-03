be9373ec5194ee77393c59535651b2d7bc381c2c
Philipkioko19-pixel <philipkioko19@example.com>
2026-09-03 19:18:59 +0300

    add

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter number: ");
+        string valuefromUser = Console.ReadLine();
+        int x = int.Parse(valuefromUser);
+        int y = 2;
+
+        if (x > y)
+
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("equal");
+        }
     }
 }
